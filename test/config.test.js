3e2dc4b0baf3aad7171406828d5305679e086b5b
fela.maslen <fela.maslen@mubaloo.com>
2017-08-24 16:15:35 +0100

    Wrote tests for config object

---
+++ test/config.test.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+const config = require('../server/config')();
+
+describe('Configuration', () => {
+    it('should be an object', () => {
+        expect(config).to.be.an('object');
+    });
+
+    it('should define vpnTable', () => {
+        expect(config.vpnTable).to.be.a('string').lengthOf.greaterThan(0);
+    });
+    it('should define subnetLan', () => {
+        expect(config.subnetLan).to.be.a('string').lengthOf.greaterThan(0);
+    });
+    it('should define toggleCmd', () => {
+        expect(config.toggleCmd).to.be.a('string').lengthOf.greaterThan(0);
+    });
+    it('should define webUsername', () => {
+        expect(config.webUsername).to.be.a('string').lengthOf.greaterThan(0);
+    });
+    it('should define webPassword', () => {
+        expect(config.webPassword).to.be.a('string').lengthOf.greaterThan(0);
+    });
+});
+
